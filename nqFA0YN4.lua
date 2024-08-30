a5bca5678bf5305c08701ee79805f3cd10f0f898
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-08-31 06:46:39 +0700

    Update nqFA0YN4.lua

--- nqFA0YN4.lua
+++ nqFA0YN4.lua
@@ -20,9 +20,7 @@ _G.AI = false
 _G.Farm = false
 _G.TP = false
 _G.Spectate = false
-local function loading()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/KOKAKAKA/Synthia/main/blademodule.lua'))()
-end
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
