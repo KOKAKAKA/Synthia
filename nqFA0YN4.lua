431d1343248aa65a7269aced20a5fd7a5e36bfbb
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-08-30 19:27:42 +0700

    Update nqFA0YN4.lua

--- nqFA0YN4.lua
+++ nqFA0YN4.lua
@@ -6,3 +6,17 @@ repeat wait() until game:IsLoaded()
 _G.Parry = false
 _G.Spam = false
 _G.Curve = false
+_G.CurveSpam = false
+_G.Shaders = false
+_G.Night = false
+_G.Visual = false
+_G.Sound = false
+_G.Effect = false
+_G.Trail = false
+_G.BallTrail = false
+_G.SelfEffect = false
+_G.KillEffect = false
+_G.AI = false
+_G.Farm = false
+_G.TP = false
+_G.Spectate = false
